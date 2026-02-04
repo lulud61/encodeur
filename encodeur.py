0a13cede02a81b96ee6734be89000193edb8ef55
lulud61 <lucasdemaillet@gmail.com>
2026-02-04 22:41:53 +0100

    fucking conversion sistem upgraded and optimised

--- encodeur.py
+++ encodeur.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 def image(nom_image):
+    global img
     img = Image.open(nom_image)
     largeur = img.width
     hauteur = img.height
@@ -8,9 +9,6 @@ def image(nom_image):
 
 
 def reset(largeur,hauteur): #rend toute les valeur rgb pair (represente le zero)
-    global r
-    global v
-    global b
     for y in range(hauteur):
         for x in range(largeur):
             L=img.getpixel((x,y))
@@ -34,11 +32,20 @@ def reset(largeur,hauteur): #rend toute les valeur rgb pair (represente le zero)
             img.putpixel( (x,y) , (new_r,new_v,new_b) )
 
 
+def changement(dico,r,v,b,x,y):
+    img.putpixel( (x,y) , (r+dico[0][0],v+dico[0][1],b+dico[0][2]) )
+    x=x+1
+    img.putpixel( (x,y) , (r+dico[1][0],v+dico[1][1],b+dico[1][2]) )
+
+
+
 def convertisseur(hauteur,largeur):
     message=input("inserer le message:")
     fin=len(message)-1
     letre=0
     stop=False
+    global x
+    global y
     y=0
     hauteur=hauteur
     largeur=largeur
@@ -46,110 +53,64 @@ def convertisseur(hauteur,largeur):
         x=0
         if stop==False:
             while x <=largeur and stop==False:
-                if message[letre-1]=="a":
-                   img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                   x=x+1
-                   img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                if message[letre]=="b":
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                if message[letre]=="c":
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                if message[letre]=="d":
-                    img.putpixel( (x,y) , (r+0,v+1,b+1) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                if message[letre]=="e":
-                    img.putpixel( (x,y) , (r+1,v+1,b+1) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                if message[letre]=="f":
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                if message[letre]=="g":
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                if message[letre]=="h":
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                if message[letre]=="i":
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                if message[letre]=="j":
-                    img.putpixel( (x,y) , (r+1,v+1,b+1) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                if message[letre]=="k":
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                if message[letre]=="l":
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                if message[letre]=="m":
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                if message[letre]=="n":
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                if message[letre]=="o":
-                    img.putpixel( (x,y) , (r+1,v+1,b+1) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                if message[letre]=="p":
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                if message[letre]=="q":
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                if message[letre]=="r":
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                if message[letre]=="s":
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                if message[letre]=="t":
-                    img.putpixel( (x,y) , (r+1,v+1,b+1) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                if message[letre]=="u":
-                    img.putpixel( (x,y) , (r+1,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+1) )
-                if message[letre]=="v":
-                    img.putpixel( (x,y) , (r+1,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+1) )
-                if message[letre]=="x":
-                    img.putpixel( (x,y) , (r+0,v+1,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+1) )
-                if message[letre]=="y":
-                    img.putpixel( (x,y) , (r+1,v+1,b+1) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+1) )
-                if message[letre]=="z":
-                    img.putpixel( (x,y) , (r+1,v+0,b+1) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+1,b+1) )
-                if message[letre]==" ":
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
-                    x=x+1
-                    img.putpixel( (x,y) , (r+0,v+0,b+0) )
+                L=img.getpixel((x,y))
+                r=int(L[0])
+                v=int(L[1])
+                b=int(L[2])
+                if message[letre]==dico["a"]:
+                    changement(dico["a",r,v,b,x,y])
+                elif message[letre]==dico["b"]:
+                    changement(dico["b",r,v,b,x,y])
+                elif message[letre]==dico["c"]:
+                    changement(dico["c",r,v,b,x,y])
+                elif message[letre]==dico["d"]:
+                    changement(dico["d",r,v,b,x,y])
+                elif message[letre]==dico["e"]:
+                    changement(dico["e",r,v,b,x,y])
+                elif message[letre]==dico["f"]:
+                    changement(dico["f",r,v,b,x,y])
+                elif message[letre]==dico["g"]:
+                    changement(dico["g",r,v,b,x,y])
+                elif message[letre]==dico["h"]:
+                    changement(dico["h",r,v,b,x,y])
+                elif message[letre]==dico["i"]:
+                    changement(dico["i",r,v,b,x,y])
+                elif message[letre]==dico["j"]:
+                    changement(dico["j",r,v,b,x,y])
+                elif message[letre]==dico["k"]:
+                    changement(dico["k",r,v,b,x,y])
+                elif message[letre]==dico["l"]:
+                    changement(dico["l",r,v,b,x,y])
+                elif message[letre]==dico["m"]:
+                    changement(dico["m",r,v,b,x,y])
+                elif message[letre]==dico["n"]:
+                    changement(dico["n",r,v,b,x,y])
+                elif message[letre]==dico["o"]:
+                    changement(dico["o",r,v,b,x,y])
+                elif message[letre]==dico["p"]:
+                    changement(dico["p",r,v,b,x,y])
+                elif message[letre]==dico["q"]:
+                    changement(dico["q",r,v,b,x,y])
+                elif message[letre]==dico["r"]:
+                    changement(dico["r",r,v,b,x,y])
+                elif message[letre]==dico["s"]:
+                    changement(dico["s",r,v,b,x,y])
+                elif message[letre]==dico["t"]:
+                    changement(dico["t",r,v,b,x,y])
+                elif message[letre]==dico["u"]:
+                    changement(dico["u",r,v,b,x,y])
+                elif message[letre]==dico["v"]:
+                    changement(dico["v",r,v,b,x,y])
+                elif message[letre]==dico["w"]:
+                    changement(dico["w",r,v,b,x,y])
+                elif message[letre]==dico["x"]:
+                    changement(dico["x",r,v,b,x,y])
+                elif message[letre]==dico["y"]:
+                    changement(dico["y",r,v,b,x,y])
+                elif message[letre]==dico["z"]:
+                    changement(dico["z",r,v,b,x,y])
+                elif message[letre]==dico[" "]:
+                    changement(dico[" ",r,v,b])
                 
                 
                 if letre >=fin:
@@ -162,10 +123,41 @@ def convertisseur(hauteur,largeur):
 
 
 
+dico={
+    "a":[[1,0,0],[0,0,0]],
+    "b":[[1,1,0],[0,0,0]],
+    "c":[[0,1,0],[0,0,0]],
+    "d":[[0,1,1],[0,0,0]],
+    "e":[[1,1,1],[0,0,0]],
+    "f":[[0,0,0],[1,0,0]],
+    "g":[[1,0,0],[1,0,0]],
+    "h":[[1,1,0],[1,0,0]],
+    "i":[[0,1,0],[1,0,0]],
+    "j":[[1,1,1],[1,0,0]],
+    "k":[[0,0,0],[1,1,0]],
+    "l":[[1,0,0],[1,1,0]],
+    "m":[[1,1,0],[1,1,0]],
+    "n":[[0,1,0],[1,1,0]],
+    "o":[[1,1,1],[1,1,0]],
+    "p":[[0,0,0],[0,1,0]],
+    "q":[[1,0,0],[0,1,0]],
+    "r":[[1,1,0],[0,1,0]],
+    "s":[[0,1,0],[0,1,0]],
+    "t":[[1,1,1],[0,1,0]],
+    "u":[[1,0,0],[0,1,1]],
+    "v":[[1,1,0],[0,1,1]],
+    "w":[[0,0,0],[1,1,1]],
+    "x":[[0,1,0],[0,1,1]],
+    "y":[[1,1,1],[0,1,1]],
+    "z":[[1,0,1],[0,1,1]],
+    " ":[[0,0,0],[0,0,0]]
+    }
+
+
 
 img,largeur,hauteur=image("frog.png")
 reset(largeur,hauteur)
 convertisseur(largeur,hauteur)
 
-
 img.show()
+print("fini")
